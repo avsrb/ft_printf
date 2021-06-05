3c1635e9e265ae269d312631755aaffce1da99c0
mshmelly <mshmelly@student.21-school.ru>
2021-06-05 11:46:46 +0300

    corected header

--- ft_printf.h
+++ ft_printf.h
@@ -25,26 +25,21 @@ typedef struct s_flag
 }				t_flag;
 
 int		ft_printf(const char *format, ...);
-
 void	ft_putchar_len(char c, t_flag *spec);
 void	ft_putstr_len(char *s, t_flag *spec);
-
 char	*ft_strchr(const char *s, int c);
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *str);
 int		ft_nbrlen(long long n, t_flag *spec);
 void	ft_zero_space(t_flag *spec, int num, int c);
-
 void	ft_putnbr(unsigned long long n, t_flag *spec);
 void	ft_init_flag(t_flag *spec);
 void	ft_parsing(const char *format, t_flag *spec, va_list ap);
 void	ft_parce_flag(const char *format, t_flag *spec);
 void	ft_parce_width(const char *format, t_flag *spec, va_list ap);
 void	ft_parce_precision(const char *format, t_flag *spec, va_list ap);
-
 int		ft_process(const char *format, t_flag *spec, va_list ap);
 void	ft_process_char(t_flag *spec, char c);
-
 void	ft_process_str(t_flag *spec, char *str);
 void	ft_process_int(t_flag *spec, long long nbr);
 void	ft_process_int_unsigned(t_flag *spec, unsigned int nbr);
